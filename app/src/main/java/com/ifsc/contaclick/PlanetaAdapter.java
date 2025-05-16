d1d556610c6937abb86abd3b5ee5397d28f9a683
Your Name <you@example.com>
2025-05-15 21:33:32 -0300

    classe planeta e dao

--- app/src/main/java/com/ifsc/contaclick/PlanetaAdapter.java
+++ app/src/main/java/com/ifsc/contaclick/PlanetaAdapter.java
@@ -1,9 +1,12 @@
 package com.ifsc.contaclick;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.List;
 
@@ -12,5 +15,12 @@ public class PlanetaAdapter extends ArrayAdapter<Planeta> {
 
     public PlanetaAdapter(@NonNull Context context, int resource, @NonNull List<Planeta> objects) {
         super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        return
     }
 }
