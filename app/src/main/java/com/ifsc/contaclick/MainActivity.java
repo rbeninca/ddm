21fa62fb29a317fe2ffd58aaedf19ec7df28b480
Romulo de Aguiar Beninca <rbeninca@gmail.com>
2025-06-11 17:06:11 -0300

    listando apps lançaveis e apps instalados pelo usuário

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -11,7 +11,9 @@ import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     PackageManager pm;
@@ -32,53 +34,92 @@ public class MainActivity extends AppCompatActivity {
         pm = getPackageManager();
 
         //informa o objeto de tratamento de eventos de toque os botões
-        btnAll.setOnClickListener(v->{atualizaListagemListView(listaLancaveis());});
+        btnAll.setOnClickListener(v->{atualizaListagemListView(listaTodosApps());});
         btnLancaveis.setOnClickListener(v->{atualizaListagemListView(listaTodosApps());});
 
         //Configura  o tratamento de evento para lançar apps da listagem
         listView.setOnItemClickListener((adapterView, view, position, id) -> {
             Aplicativo aplicativo = (Aplicativo) adapterView.getItemAtPosition(position);
+            //recupera uma intenção de lançamento do aplicativo selecionado  por meio do PackageManager
             Intent launchIntent = pm.getLaunchIntentForPackage(aplicativo.packageName);
+            //Se a intenção de lançamento não for nula, inicia a atividade do aplicativo
             if (launchIntent != null) {
                 startActivity(launchIntent);
             }
         });
-
+        btnAll.performClick();
     }
     public  ArrayList<Aplicativo> listaTodosApps() {
         listaAplicativos.clear(); // Limpa a lista antes de adicionar novos aplicativos
+        /* O método getInstalledApplications do PackageManager retorna uma lista de objetos ApplicationInfo
+           que representam os aplicativos instalados no dispositivo. Cada ApplicationInfo contém informações sobre o aplicativo,
+           como nome, ícone, pacote, etc.
+
+           Mas podemos aplicar um filtro para obter apenas aplicativos específicos, como aplicativos de terceiros ou aplicativos do sistema.
+        ria um filtro da lista de applicationInfor por meio do atributo ApplicationInfo.flags
+           que pode ser usado para filtrar aplicativos do sistema, aplicativos de terceiros, bibliotecas, etc.
+           O filtro GET_META_DATA e GET_SHARED_LIBRARY_FILES são usados para incluir metadados e bibliotecas compartilhadas.
+
+           flags é um conjunto de bits que pode ser usado para filtrar aplicativos com base em suas características.
+           Exemplo
+              Constante                                 Int decimal       binário
+              PackageManager.MATCH_ALL:                0x00000000        00000000 00000000 00000000 00000000
+              PackageManager.GET_SHARED_LIBRARY_FILES: 0x00000020        00000000 00000000 00000000 00100000
+              PackageManager.GET_META_DATA:            0x00000080        00000000 00000000 00000000 10000000
+              PackageManager.GET_ACTIVITIES:           0x00000001        00000000 00000000 00000000 00000001
+              PackageManager.GET_SERVICES:             0x00000004        00000000 00000000 00000000 00000100
+              PackageManager.FLAG_SYSTEM:              0x00000001        00000000 00000000 00000000 00000001
+              ...
+
+              Portanto, o filtro combinado seria:
+            app.Flag fosse                  0x00000001        00000000 00000000 00000000 00000001
+            &
+            (ApplicationInfo.FLAG_SYSTEM)   0x00000001        00000000 00000000 00000000 00000001
+            =                               0x00000000        00000000 00000000 00000000 00000000
+              Isso significa que estamos filtrando apenas aplicativos de terceiros, ou seja, aqueles que não são parte do sistema operacional.
+
+            Exemplo se quiser incluir aplicativos do sistema, podemos usar:
+            int flags = app.flags & (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP);
+            0x00000001 | 0x00000002 = 0x00000003
+            Isso incluiria aplicativos do sistema e aplicativos atualizados do sistema.
+
+              Se quisermos incluir todos os aplicativos, podemos usar:
+            int flags = app.flags & (ApplicationInfo.FLAG_SYSTEM | ApplicationInfo.FLAG_UPDATED_SYSTEM_APP | ApplicationInfo.FLAG_INSTALLED);
+            0x00000001 | 0x00000002 | 0x00000004 = 0x00000007
+
+         */
 
         //Recupera a lista de aplicativos instalados inclundo os aplicativos do sistema, bibliotecas e aplicativos de terceiros...
-        for (ApplicationInfo app : pm.getInstalledApplications(PackageManager.GET_META_DATA)) {
-            if ((app.flags & android.content.pm.ApplicationInfo.FLAG_SYSTEM) == 0) {
+
+
+        Intent launcherIntent = new Intent(Intent.ACTION_MAIN, null);
+        launcherIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> launchables = pm.queryIntentActivities(launcherIntent, PackageManager.MATCH_ALL);
+
+
+        Set<String> pacoteLancavel = new HashSet<>();
+        for (ResolveInfo info : launchables) {
+            pacoteLancavel.add(info.activityInfo.packageName);
+        }
+
+
+
+        for (ApplicationInfo app : pm.getInstalledApplications(PackageManager.MATCH_ALL)) {
+             //verifica se a aplicação tem uma activity que responde a  intenção a Intent.ACTION_MAIN
+                // e se o pacote está na lista de pacotes lançávei
+            if (!pacoteLancavel.contains(app.packageName)) continue;
                 Aplicativo aplicativo = new Aplicativo();
                 aplicativo.nome = app.loadLabel(pm).toString();
                 aplicativo.resolveInfo = pm.resolveActivity(new Intent(Intent.ACTION_MAIN).setPackage(app.packageName), PackageManager.MATCH_DEFAULT_ONLY);
                 aplicativo.packageName = app.packageName;
                 aplicativo.icone = app.loadIcon(pm);
                 listaAplicativos.add(aplicativo);
-            }
+
         }
         return listaAplicativos;
     }
-    public ArrayList<Aplicativo> listaLancaveis() {
-        listaAplicativos.clear(); // Limpa a lista antes de adicionar novos aplicativos
-        //recupera os aplicativos que podem ser lançados
-        Intent intent = new Intent(Intent.ACTION_MAIN, null);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        List<ResolveInfo> appsInfo=(ArrayList<ResolveInfo>) pm.queryIntentActivities(intent, 0);
 
-        for (ResolveInfo resolveInfo : appsInfo) {
-            Aplicativo aplicativo = new Aplicativo();
-            aplicativo.nome = resolveInfo.loadLabel(pm).toString();
-            aplicativo.resolveInfo = resolveInfo;
-            aplicativo.packageName = resolveInfo.activityInfo.packageName;
-            aplicativo.icone = resolveInfo.loadIcon(pm);
-            listaAplicativos.add(aplicativo);
-        }
-        return listaAplicativos;
-
-    }
     public void atualizaListagemListView(ArrayList<Aplicativo> listapps) {
         AppAdapter adapter = new AppAdapter(this, R.layout.app_item, listapps);
         listView.setAdapter(adapter);
