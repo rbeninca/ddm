cf90d8ec79e206890f840b61b9ae3c573d6dd11a
Romulo de Aguiar Beninca <rbeninca@gmail.com>
2025-06-27 11:01:28 -0300

    mapa com osmdroid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("org.osmdroid:osmdroid-android:6.1.16")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ifsc.contaclick;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
@@ -9,20 +10,38 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvLatitude, tvLongitude, tvStatus;
     LocationManager locationManager;
+    private MapView map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Configuration.getInstance().setUserAgentValue(getPackageName());
         setContentView(R.layout.activity_main);
         tvLatitude = findViewById(R.id.tvLatitude);
         tvLongitude = findViewById(R.id.tvLongitude);
         tvStatus = findViewById(R.id.tvStatus);
         Button button= findViewById(R.id.button);
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMultiTouchControls(true);
+
+
+
         button.setOnClickListener(v -> {
            getLocalizacao();
         });
@@ -30,9 +49,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    @SuppressLint("MissingPermission")
     private void getLocalizacao() {
         if (checkAndGetPermissions()) {
           locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
         } else {
             tvStatus.setText("Permissão Negada");
         }
@@ -49,12 +71,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    private void showUserLocation(double latitude, double longitude) {
+        GeoPoint userLocation = new GeoPoint(latitude, longitude);
+        // Centraliza e ajusta zoom no ponto recebido
+        map.getController().setCenter(userLocation);
+        map.getController().setZoom(18.0);
+
+        // Adiciona um marcador no ponto
+        Marker marker = new Marker(map);
+        marker.setPosition(userLocation);
+        marker.setTitle("Você está aqui");
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().clear();
+        map.getOverlays().add(marker);
+
+
+        map.invalidate(); // Atualiza visualmente
+
+
+    }
     public final android.location.LocationListener locationListener = new android.location.LocationListener() {
         @Override
         public void onLocationChanged(@NonNull Location location) {
             tvLatitude.setText("Latitude: " + location.getLatitude());
             tvLongitude.setText("Longitude: " + location.getLongitude());
             tvStatus.setText("Localização Atualizada");
+            showUserLocation(location.getLatitude(), location.getLongitude());
         }
 
         @Override
