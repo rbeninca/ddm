a3d6c2d80cfbb18173e59e4079b899d41856c049
Romulo de Aguiar Beninca <rbeninca@gmail.com>
2025-06-27 19:51:13 -0300

    funcção seguir localição implementada

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -6,22 +6,24 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.text.DecimalFormat;
 
@@ -42,8 +44,23 @@ public class MainActivity extends AppCompatActivity {
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setMultiTouchControls(true);
 
+        map.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                isUserInteracting = true;
+                tvStatus.setText("Interagindo com o mapa");
+            }
+            return false; // deixa o MapView continuar tratando o toque normalmente
+        });
 
 
+        tvStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvStatus.setText("Seguindo Localização");
+                seguirCoordenadas = true;
+                isUserInteracting=false;
+            }
+        });
         button.setOnClickListener(v -> {
            getLocalizacao();
         });
@@ -65,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean checkAndGetPermissions() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PackageManager.PERMISSION_GRANTED);
             return false;
         } else {
@@ -73,34 +90,55 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-    private boolean firstLocationUpdate = true;
+
+    private boolean seguirCoordenadas = true;
 
     private void showUserLocation(double latitude, double longitude) {
         GeoPoint userLocation = new GeoPoint(latitude, longitude);
-
-        if (firstLocationUpdate) {
+        if (seguirCoordenadas && !isUserInteracting) {
             map.getController().setCenter(userLocation);
             map.getController().setZoom(18.0);
-            firstLocationUpdate = true;
+            if (!isUserInteracting) {
+                map.getController().animateTo(userLocation);
+            }
         }
-
         Marker marker = new Marker(map);
         marker.setPosition(userLocation);
         marker.setTitle("Você está aqui");
+
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
         map.getOverlays().clear();
         map.getOverlays().add(marker);
 
         map.invalidate();
     }
+
+    private boolean isUserInteracting = false;
+
+
+
+    public void showPonintInMap(double latitude, double longitude) {
+        GeoPoint point = new GeoPoint(latitude, longitude);
+        IMapController mapController = map.getController();
+        mapController.setZoom(18.0);
+        mapController.setCenter(point);
+
+        Marker marker = new Marker(map);
+        marker.setPosition(point);
+        marker.setTitle("Localização");
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(marker);
+        map.invalidate();
+
+
+    }
+
     public final android.location.LocationListener locationListener = new android.location.LocationListener() {
         @Override
         public void onLocationChanged(@NonNull Location location) {
             DecimalFormat decimalFormat = new DecimalFormat("##.######");
             tvLatitude.setText("Latitude: " + decimalFormat.format(location.getLatitude()));
             tvLongitude.setText("Longitude: " + decimalFormat.format(location.getLongitude()));
-
-            tvStatus.setText("Localização Atualizada");
             showUserLocation(location.getLatitude(), location.getLongitude());
         }
 
