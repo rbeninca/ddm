88d7111273496fb8335e2d179f5d81c39206f06c
unknown <you@example.com>
2025-06-26 22:08:43 -0300

    gps locationManager

--- app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -1,44 +1,72 @@
 package com.ifsc.contaclick;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import java.util.Random;
+public class MainActivity extends AppCompatActivity  {
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
-
-    SensorManager sensorManager;
-    TextView tv;
+    LocationManager lm;
+    TextView tvLatitude, tvLongitude;
+    Button bnt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv=findViewById(R.id.tvResultado);
-        sensorManager=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor sensor1=sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        Sensor sensor2=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this,sensor1,SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this,sensor2,SensorManager.SENSOR_DELAY_NORMAL);
+        tvLatitude=findViewById(R.id.tvLatitude);
+        tvLongitude=findViewById(R.id.tvLongitude);
+        bnt=findViewById(R.id.button);
+
+        lm=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        bnt.setOnClickListener(v->getLocalizcao());
     }
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if(sensorEvent.sensor.getType()==Sensor.TYPE_LIGHT) {
-            tv.setText(Float.toString(sensorEvent.values[0]));
+
+    public void getLocalizcao() {
+
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) && (
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                        PackageManager.PERMISSION_GRANTED)
+
+        ) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    1);
+            return;
         }
+
+        Location location;
+        location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+        if (location!=null){
+            tvLatitude.setText(  Double.toString( location.getLatitude()  ) );
+            tvLongitude.setText(  Double.toString( location.getLongitude()  ) );
+        } else{
+            tvLatitude.setText(  "erro" );
+        }
+
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if ( requestCode==1){
+            getLocalizcao();
+        }
     }
 }
